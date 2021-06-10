8fc97c1afd940ba31795ee8697136acfbe6e2af0
aadvik-coder <80146416+aadvik-coder@users.noreply.github.com>
2021-06-10 22:40:25 +1000

    Add files via upload

---
+++ chat_page.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyA4floLwDUG2YTLk1WwwHIEhMiVduix7-4",
+    authDomain: "chatfriends-2ac34.firebaseapp.com",
+    databaseURL: "https://chatfriends-2ac34-default-rtdb.firebaseio.com",
+    projectId: "chatfriends-2ac34",
+    storageBucket: "chatfriends-2ac34.appspot.com",
+    messagingSenderId: "770548234874",
+    appId: "1:770548234874:web:8d6e7ed22e6064b4bb59c3",
+    measurementId: "G-HKTWT3FXQ8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
